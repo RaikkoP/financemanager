b670756aa3175494909c9d31cb839c1bbc8a72c0
RaikkoP <raikko.prants@gmail.com>
2023-11-03 16:17:05 +0200

    Added testing for backend expenses

--- FinanceAPI/server.js
+++ FinanceAPI/server.js
@@ -12,6 +12,72 @@ const db = mysql.createConnection({
     database: process.env.DATABASE
 })
 
+//Added test account with data
+app.get('/test', (req, res) => {
+    const sql = 
+    "INSERT INTO Users (username, password) VALUES('test5', 'test5')"
+    db.query(sql, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Created user');
+    })
+    const jsonData = JSON.stringify([
+        {
+            id: 1,
+            name: "New Jacket",
+            type: "Expense",
+            amount: "200$",
+            date: "02.02.2023"
+        },
+        {
+            id: 2,
+            name: "New Socks",
+            type: "Expense",
+            amount: "20$",
+            date: "02.02.2023"
+        },
+        {
+            id: 3,
+            name: "Mom Paid Me",
+            type: "Income",
+            amount: "200$",
+            date: "02.02.2023"
+        },
+        {
+            id: 4,
+            name: "Salary",
+            type: "Income",
+            amount: "900$",
+            date: "02.02.2023"
+        }
+    ]);
+    const sql2 =
+    `INSERT INTO UserData (userId, data) SELECT id, '${jsonData}' AS data FROM Users WHERE username = 'test5'`
+    db.query(sql2, (err, data) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('Created data')
+    })
+})
+
+app.get('/get/finance', (req, res) => {
+    const sql = "SELECT data FROM UserData WHERE userId = (SELECT id FROM Users WHERE username = 'test5')";
+    db.query(sql, (err, data) => {
+        if(err) {
+            console.log(err);
+        }
+        if(data.length > 0) {
+            res.send(JSON.parse(data[0].data));
+        }
+    });
+})
+
+app.post('/create/finance', (req, res) => {
+
+});
+
 app.get('/', (req, res) => {
     return console.log('Whats good!')
 })
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,40 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 import Filters from "../Filters/Filters";
 import Form from "../Form/Form";
 import Header from "../Header/Header";
 
 export default function Dashboard()  {
 
-    const [data, setData] = useState([
-        {
-            id: 1,
-            name: "New Jacket",
-            type: "Expense",
-            amount: "200$",
-            date: "02.02.2023"
-        },
-        {
-            id: 2,
-            name: "New Socks",
-            type: "Expense",
-            amount: "20$",
-            date: "02.02.2023"
-        },
-        {
-            id: 3,
-            name: "Mom Paid Me",
-            type: "Income",
-            amount: "200$",
-            date: "02.02.2023"
-        },
-        {
-            id: 4,
-            name: "Salary",
-            type: "Income",
-            amount: "900$",
-            date: "02.02.2023"
-        }
-    ]);
+    const [data, setData] =  useState([]);
+
+    async function getData() {
+        axios.get('http://localhost:5000/get/finance')
+            .then(function (response) {
+                console.log(response);
+                setData([...data, ...response.data]);
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
+    useEffect(() => {
+        getData();
+    },[])
     
     return (
         <>
